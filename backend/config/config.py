f1ca80661291b2d14d26d3a2cdcb4fcd7a9371eb
Leo Cardhio <leocardhio@gmail.com>
2026-03-24 23:02:30 +0700

    feat(pgsql-setup): setup docker for psql

--- backend/config/config.py
+++ backend/config/config.py
@@ -3,5 +3,10 @@ from typing import List
 
 class Config(BaseSettings):
   valid_content_types: List[str] = []
+  postgres_user: str
+  postgres_password: str
+  postgres_db: str
+  postgres_host: str
+  postgres_port: str
 
   model_config = SettingsConfigDict(env_file='.env')
