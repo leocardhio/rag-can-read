d92bb4ae3fb3e39b8ce69c1bb58a067d4fad364f
Leo <leocardhio@gmail.com>
2026-04-10 16:54:29 +0700

    chore(eslint): enable prettier eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,6 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      'semi': ['error', 'always'],
+      'trailing-comma': ['error', 'always-multiline'],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
